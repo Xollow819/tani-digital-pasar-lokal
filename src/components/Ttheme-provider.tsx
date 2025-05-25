910d808ac7b991eafb850186d68ecc5636b17d00
Muhammad Fauzan Lazwar <67082143+Xollow819@users.noreply.github.com>
2025-05-25 22:36:16 +0700

    Create Ttheme-provider.tsx

---
+++ src/components/Ttheme-provider.tsx
@@ -0,0 +1,8 @@
+// src/components/theme-provider.tsx
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import type { ThemeProviderProps } from "next-themes/dist/types";
+import * as React from "react";
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
